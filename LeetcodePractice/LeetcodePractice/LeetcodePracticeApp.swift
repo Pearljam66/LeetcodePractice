a27fddf764e28c3b84dec44962d5818f61fd8719
Sarah Clark <sarahireneclark@gmail.com>
2024-04-27 03:51:52 -0500

    Added LeetcodePractice project to using unit test capability with leetcode problems

---
+++ LeetcodePractice/LeetcodePractice/LeetcodePracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeetcodePracticeApp.swift
+//  LeetcodePractice
+//
+//  Created by Sarah Clark on 4/27/24.
+//
+
+import SwiftUI
+
+@main
+struct LeetcodePracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
