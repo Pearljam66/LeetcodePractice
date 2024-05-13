de72df5e1356cc22f0eb6ce8ff4913f0bbfda6cf
Sarah Clark <sarahireneclark@gmail.com>
2024-05-13 11:53:49 -0500

    Added Binary Search Tree code

---
+++ LeetcodePractice/LeetcodePracticeTests/BinarySearchTrees/BinarySearchTree.swift
@@ -0,0 +1,44 @@
+//
+//  BinarySearchTree.swift
+//  LeetcodePracticeTests
+//
+//  Created by Sarah Clark on 5/13/24.
+//
+
+public struct BinarySearchTree<Element: Comparable> {
+
+    public private(set) var root: BinaryNodeTwo<Element>?
+
+    public init() {}
+}
+
+extension BinarySearchTree: CustomStringConvertible {
+
+    public var description: String {
+        guard let root = root else { return "empty tree" }
+        return String(describing: root)
+    }
+
+}
+
+extension BinarySearchTree {
+
+    public mutating func insert(_ value: Element) {
+        root = insert(from: root, value: value)
+    }
+
+    private func insert(from node: BinaryNodeTwo<Element>?, value: Element) -> BinaryNodeTwo<Element> {
+
+        guard let node = node else {
+            return BinaryNodeTwo(value: value)
+        }
+
+        if value < node.value {
+            node.leftChild = insert(from: node.leftChild, value: value)
+        } else {
+            node.rightChild = insert(from: node.rightChild, value: value)
+        }
+
+        return node
+    }
+}
