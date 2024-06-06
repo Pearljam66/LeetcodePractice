c73d1592ca62a9e49400cb8cad8d700c9cf4730c
Sarah Clark <sarahireneclark@gmail.com>
2024-06-06 15:13:36 -0500

    Fixed the spelling

--- LeetcodePractice/LeetcodePracticeTests/ValidParenthesesTests.swift
+++ LeetcodePractice/LeetcodePracticeTests/ValidParenthesesTests.swift
@@ -1,5 +1,5 @@
 //
-//  ValidParethesesTests.swift
+//  ValidParenthesesTests.swift
 //  LeetcodePracticeTests
 //
 //  Created by Sarah Clark on 6/5/24.
@@ -34,7 +34,7 @@
 
 import XCTest
 
-final class ValidParethesesTests: XCTestCase {
+final class ValidParenthesesTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
